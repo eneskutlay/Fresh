405079ef6da5f92c5c5f251e2f9f288daead6633
Enes Kutlay <20182542@std.neu.edu.tr>
2020-12-17 00:50:34 +0300

    correcting

--- root/index.js
+++ root/index.js
@@ -1,35 +1,47 @@
 var input = document.getElementById("input");
 var btn = document.getElementById("btn");
 var tagLi = document.getElementsByTagName("li");
+var specialKeys = ["!clear"];
 
 input.addEventListener("keyup", function (event) {
-	if (event.keyCode === 13) {
-		event.preventDefault();
-		btn.click();
-	}
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    btn.click();
+  }
 });
 
 function addToDo() {
-	var li = document.createElement("li");
-	li.id = "idli"
-	var inputValue = document.getElementById("input").value;
-	var t = document.createTextNode(inputValue);
-	li.appendChild(t);
-	if (inputValue === "") {
-		console.log("alert");
-	} else if (tagLi.length <= 14) {
-		document.getElementById("list").appendChild(li);
-		document.getElementById("input").value = "";
-	} else if (tagLi.length > 14) {
-		alert("Önce yapılacakları bitir");
-	}
-	li.addEventListener("click", /* finifshTodo */ );
+  var li = document.createElement("li");
+  li.id = "idli";
+  var inputValue = document.getElementById("input").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  checkItem();
+  addItem();
+
+  ///// Actions
+  function checkItem() {
+    if (inputValue == "!clear") {
+      alert("itemleri temizle");
+    } else {
+    }
+  }
+  function addItem() {
+    if (tagLi.length <= 13 && inputValue != specialKeys) {
+      document.getElementById("list").appendChild(li);
+      document.getElementById("input").value = "";
+    } else {
+      alert("önce yapılacakları bitir");
+    }
+  }
 }
 
 /* not working function finifshTodo() {
 document.querySelectorAll("li").this.style.textDecoration = "line-through";
-} */
+} 
+li.addEventListener("click",  finifshTodo  );
+*/
 
 function addBtn() {
-	addToDo();
+  addToDo();
 }
